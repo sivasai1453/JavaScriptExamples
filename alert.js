6e9a99b944ea53d57eea722b8dd00c86d5241f5c
sivasai1453 <sivasaia1453@gmail.com>
2025-02-24 20:26:39 +0530

    password generator in index.js file is add

--- alert.js
+++ alert.js
@@ -1,3 +1,3 @@
-function msg(){
-    alert("sai");
-}
+let username=window.prompt("enter you name");
+username= username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLocaleLowerCase();
+console.log(username);
--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-function greeting(name){
-  if(name==="siva")   
-{
-    var greet=`hello ${name}`;
+
+function generatePassword(length,includeLowerCase,includeUpperCase,includeNumbers,includeSymbols) {
+  
+  const lowercase="abcdefghijklmnopqrstuvwxyz";
+  const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers="0123456789";
+  const symbolschar="!@#$%^&*()_+"; 
+
+  let allowchar="";
+  let password="";
+
+  allowchar+=includeLowerCase ? lowercase :"";
+  allowchar+=includeUpperCase ? uppercase :"";
+  allowchar+=includeNumbers ? numbers : "";
+  allowchar+=includeSymbols ? symbolschar : "";
+
+ if (length<=0) {
+  return `(password length must be aleast 1)`
+ }
+ if (allowchar.length===0) {
+  return `(atleast 1 set of char should be selected )`
+ }
+ for (let i = 0; i < length; i++) {
+  const random=Math.floor( Math.random() * allowchar.length);
+  password += allowchar[random];
+  
+ }
+     return password;
 }
-else{
-    var greet="hello there";
-}
-      console.log(greet)
-    }
-   
-greeting("sivasai");
+
+const lengthOFpassword =12;
+const includeLowerCase=true;
+const includeUpperCase=true;
+const includeNumbers=true;
+const includeSymbols=true;
+
+const password = generatePassword(lengthOFpassword,includeLowerCase,includeUpperCase,includeNumbers,includeSymbols);
+
+console.log(`generated password ${password}`);
